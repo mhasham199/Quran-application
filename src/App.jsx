ebc191ab4cd2db6427a83bfd3e36a1d3d1520a98
Adilahmad125 <adilahmedkhan587@gmail.com>
2025-01-27 10:37:07 +0500

    forth change

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // Remove BrowserRouter import
 import LandingPage from "./pages/LandingPage";
 import SurahPage from "./pages/SurahPage";
 import "./styles/Common.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/surah/:id" element={<SurahPage />} />
-      </Routes>
-    </Router>
+    <Routes>  {/* Directly use Routes here */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/surah/:id" element={<SurahPage />} />
+    </Routes>
   );
 }
 
